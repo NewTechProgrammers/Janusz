74af6c452fadf46640c8d069dbb034a1cf6c72a5
checkthistape <litos.art2006@gmail.com>
2024-05-29 18:03:05 +0200

    All Arduino Mega motor ports have been defined

--- project/I2Ccommunication/Slave/Slave.ino
+++ project/I2Ccommunication/Slave/Slave.ino
@@ -13,18 +13,22 @@
 #include <Wire.h>
 
 // Left Part | Forward part
-int motorA1 = 16;
-int motorA2 = 5;
-int motorAPWM = 15;
+int motorA1 = 13;
+int motorA2 = 12;
+int motorAPWM = 11;
 
-int motorB1 = 4;
-int motorB2 = 0;
-int motorBPWM = 2;
+int motorB1 = 10;
+int motorB2 = 9;
+int motorBPWM = 8;
 
 // Right Part || Backward part
-int motorBB1 = 14;
-int motorBB2 = 12;
-int motorBBPWM = 13;
+int motorAA1 = 7;
+int motorAA2 = 6;
+int motorAAPWM = 5;
+
+int motorBB1 = 4;
+int motorBB2 = 3;
+int motorBBPWM = 2;
 
 void setup() {
   Serial.begin(9600);
@@ -34,6 +38,7 @@ void setup() {
   Serial.print("Slave Ready");
 
   // Motors
+  // left side
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorAPWM, OUTPUT);
@@ -41,6 +46,11 @@ void setup() {
   pinMode(motorB1, OUTPUT);
   pinMode(motorB2, OUTPUT);
   pinMode(motorBPWM, OUTPUT);
+
+  // right side
+  pinMode(motorAA1, OUTPUT);
+  pinMode(motorAA2, OUTPUT);
+  pinMode(motorAAPWM, OUTPUT);
 
   pinMode(motorBB1, OUTPUT);
   pinMode(motorBB2, OUTPUT);
@@ -54,6 +64,9 @@ void setup() {
   digitalWrite(motorB2, LOW);
 
   // Right Part || Backward part
+  digitalWrite(motorAA1, HIGH);
+  digitalWrite(motorAA2, LOW);
+
   digitalWrite(motorBB1, HIGH);
   digitalWrite(motorBB2, LOW);
 }
@@ -102,5 +115,43 @@ void receiveEvent(size_t howMany) {
 
   if (x == 102) {
     Serial.println("DAMN!!!");
+
+    // left side
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, HIGH);
+
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, HIGH);
+
+    analogWrite(motorAPWM, 127);
+    analogWrite(motorBPWM, 127);
+
+    // right side
+
+    digitalWrite(motorBB1, LOW);
+    digitalWrite(motorBB2, HIGH);
+
+    analogWrite(motorAAPWM, 127);
+    analogWrite(motorBBPWM, 127);
+
+  }
+  if (x == 101) {
+    // left side
+    digitalWrite(motorA1, HIGH);
+    digitalWrite(motorA2, LOW);
+
+    digitalWrite(motorB1, HIGH);
+    digitalWrite(motorB2, LOW);
+
+    analogWrite(motorAPWM, 0);
+    analogWrite(motorBPWM, 0);
+
+    // right side
+
+    digitalWrite(motorBB1, HIGH);
+    digitalWrite(motorBB2, LOW);
+
+    analogWrite(motorAAPWM, 0);
+    analogWrite(motorBBPWM, 0);
   }
 }
