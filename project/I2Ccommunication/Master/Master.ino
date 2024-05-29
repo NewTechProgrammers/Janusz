4e112e4f225fa801f7d3937e660d5887c7d32ad0
checkthistape <litos.art2006@gmail.com>
2024-05-29 17:09:15 +0200

    Package content depends on a button

--- project/I2Ccommunication/Master/Master.ino
+++ project/I2Ccommunication/Master/Master.ino
@@ -10,17 +10,176 @@
 // This example code is in the public domain.
 
 
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <AsyncTCP.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+
 #include <Wire.h>
 #include <iostream>
 
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "Janusz";
+const char* password = "11111111";
+
+const int output = 5;
+
 byte x = 0;
 
-void setup() {     
+// HTML web page
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+  <head>
+    <title>ESP Pushbutton Web Server</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body { font-family: Arial; text-align: center; margin:0px auto; padding-top: 30px;}
+      .button {
+        padding: 10px 80px;
+        margin: 25px;
+        height: 60px;
+        font-size: 24px;
+        text-align: center;
+        outline: none;
+        color: #fff;
+        background-color: #2f4468;
+        border: none;
+        border-radius: 5px;
+        box-shadow: 0 6px #999;
+        cursor: pointer;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+      }  
+      .button:hover {background-color: #1f2e45}
+      .button:active {
+        background-color: #1f2e45;
+        box-shadow: 0 4px #666;
+        transform: translateY(2px);
+      }
+    </style>
+  </head>
+  <body>
+    <h1>ESP Pushbutton Web Server</h1>
+    <button class="button" id="btn1" onmousedown="toggleCheckbox('on');" ontouchstart="toggleCheckbox('on');" onmouseup="toggleCheckbox('off');" ontouchend="toggleCheckbox('off');">Forward</button>
+    <button class="button" id="btn2" onmousedown="toggleCheckbox('back');" ontouchstart="toggleCheckbox('back');" onmouseup="toggleCheckbox('off');" ontouchend="toggleCheckbox('off');">Backward</button>
+
+   <script>
+   function toggleCheckbox(x) {
+     var xhr = new XMLHttpRequest();
+     xhr.open("GET", "/" + x, true);
+     xhr.send();
+   }
+  </script>
+  </body>
+</html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+AsyncWebServer server(80);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
   Wire.begin(4,5); // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
   delay(100);
   Serial.println("Master Ready");
+
+
+  //Serial.begin(115200);
+  Serial.println("\n\nyo");
+
+  Serial.print("Setting AP (Access Point)…");
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP()); // 192.168.4.1
+  
+  
+  //pinMode(output, OUTPUT); // led
+
+  //digitalWrite(output, LOW);
+
+  // initialize digital pin LED_BUILTIN as an output.
+  
+
+  // Send web page to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  // Receive an HTTP GET request
+  server.on("/on", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    //digitalWrite(output, HIGH);
+    // analogWrite(motorAPWM, 127);
+    // analogWrite(motorBPWM, 127);
+    
+    // analogWrite(motorBBPWM, 127);
+
+    transmit('f');
+
+    request->send(200, "text/plain", "ok");
+  });
+
+  server.on("/back", HTTP_GET, [] (AsyncWebServerRequest *request){
+    // digitalWrite(motorA1, LOW);
+    // digitalWrite(motorA2, HIGH);
+
+    // digitalWrite(motorB1, LOW);
+    // digitalWrite(motorB2, HIGH);
+
+    // digitalWrite(motorBB1, LOW);
+    // digitalWrite(motorBB2, HIGH);
+
+
+    // analogWrite(motorAPWM, 127);
+    // analogWrite(motorBPWM, 127);
+
+    // analogWrite(motorBBPWM, 127);
+    transmit('b');
+    request->send(200, "text/plain", "ok");
+  });
+
+  // Receive an HTTP GET request
+  server.on("/off", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    // digitalWrite(motorA1, HIGH);
+    // digitalWrite(motorA2, LOW);
+
+    // digitalWrite(motorB1, HIGH);
+    // digitalWrite(motorB2, LOW);
+
+    // digitalWrite(motorBB1, HIGH);
+    // digitalWrite(motorBB2, LOW);
+
+
+    // //digitalWrite(output, LOW);
+    // analogWrite(motorAPWM, 0);
+    // analogWrite(motorBPWM, 0);
+
+    // analogWrite(motorBBPWM, 0);
+    transmitEnd();
+    request->send(200, "text/plain", "ok");
+  });
+  
+  server.onNotFound(notFound);
+  server.begin();
 }
+
 
 void loop() {
   // Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
@@ -30,21 +189,35 @@ void loop() {
   //   Serial.print(c);         // print the character
   // }
   // Serial.println();
-  // delay(500);
+  // delay(500); 
 
-  Wire.beginTransmission(8);
+}
+
+void transmitDefault() {
+   Wire.beginTransmission(8);
   Wire.write("x is ");
   Wire.write(x);
   Wire.endTransmission();
 
   x++;
-
-  delay(500);
-
   std::cout << "\n";
-
+  
 }
 
-void transmit() {
+void transmit(char direction) {
+  Wire.beginTransmission(8);
+  Wire.write(direction);
+  Wire.endTransmission();
+
+  std::cout << "direction " << direction << "\n";
+  
+}
+
+void transmitEnd() {
+   Wire.beginTransmission(8);
+  Wire.write('e');
+  //Wire.write(x);
+  Wire.endTransmission();
+  std::cout << "\n";
   
 }
--- project/I2Ccommunication/Slave/Slave.ino
+++ project/I2Ccommunication/Slave/Slave.ino
@@ -12,12 +12,50 @@
 
 #include <Wire.h>
 
+// Left Part | Forward part
+int motorA1 = 16;
+int motorA2 = 5;
+int motorAPWM = 15;
+
+int motorB1 = 4;
+int motorB2 = 0;
+int motorBPWM = 2;
+
+// Right Part || Backward part
+int motorBB1 = 14;
+int motorBB2 = 12;
+int motorBBPWM = 13;
+
 void setup() {
   Serial.begin(9600);
-  Wire.begin(8);                // join i2c bus with address #8
+  Wire.begin(8);  // join i2c bus with address #8
   //Wire.onRequest(requestEvent); // register event
   Wire.onReceive(receiveEvent);
   Serial.print("Slave Ready");
+
+  // Motors
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2, OUTPUT);
+  pinMode(motorAPWM, OUTPUT);
+
+  pinMode(motorB1, OUTPUT);
+  pinMode(motorB2, OUTPUT);
+  pinMode(motorBPWM, OUTPUT);
+
+  pinMode(motorBB1, OUTPUT);
+  pinMode(motorBB2, OUTPUT);
+  pinMode(motorBBPWM, OUTPUT);
+
+  // Left Part || Forward part
+  digitalWrite(motorA1, HIGH);
+  digitalWrite(motorA2, LOW);
+
+  digitalWrite(motorB1, HIGH);
+  digitalWrite(motorB2, LOW);
+
+  // Right Part || Backward part
+  digitalWrite(motorBB1, HIGH);
+  digitalWrite(motorBB2, LOW);
 }
 
 void loop() {
@@ -31,12 +69,38 @@ void loop() {
 //   // as expected by master
 // }
 
+//     analogWrite(motorAPWM, 127);
+//     analogWrite(motorBPWM, 127);
+
+//     analogWrite(motorBBPWM, 127);
+
+//     digitalWrite(motorA1, LOW);
+//     digitalWrite(motorA2, HIGH);
+
+//     digitalWrite(motorB1, LOW);
+//     digitalWrite(motorB2, HIGH);
+
+//     digitalWrite(motorBB1, LOW);
+//     digitalWrite(motorBB2, HIGH);
+
+
+//     analogWrite(motorAPWM, 127);
+//     analogWrite(motorBPWM, 127);
+
+//     analogWrite(motorBBPWM, 127);
+
+
+
 void receiveEvent(size_t howMany) {
   (void)howMany;
-  while(1 < Wire.available()){
+  while (1 < Wire.available()) {
     char c = Wire.read();
     Serial.print(c);
   }
   int x = Wire.read();
   Serial.println(x);
+
+  if (x == 102) {
+    Serial.println("DAMN!!!");
+  }
 }
