6ea9d4de8831283d37b13c94100a0dd06c2bc42c
checkthistape <litos.art2006@gmail.com>
2024-06-10 18:52:06 +0200

    Improved control, changed ports

--- project/I2Ccommunication/Master/Master.ino
+++ project/I2Ccommunication/Master/Master.ino
@@ -32,57 +32,146 @@ byte x = 0;
 
 // HTML web page
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-  <head>
+<!DOCTYPE HTML>
+<html>
+
+<head>
     <title>ESP Pushbutton Web Server</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-      body { font-family: Arial; text-align: center; margin:0px auto; padding-top: 30px;}
-      .button {
-        padding: 10px 80px;
-        margin: 25px;
-        height: 60px;
-        font-size: 24px;
-        text-align: center;
-        outline: none;
-        color: #fff;
-        background-color: #2f4468;
-        border: none;
-        border-radius: 5px;
-        box-shadow: 0 6px #999;
-        cursor: pointer;
-        -webkit-touch-callout: none;
-        -webkit-user-select: none;
-        -khtml-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        -webkit-tap-highlight-color: rgba(0,0,0,0);
-      }  
-      .button:hover {background-color: #1f2e45}
-      .button:active {
-        background-color: #1f2e45;
-        box-shadow: 0 4px #666;
-        transform: translateY(2px);
-      }
-    </style>
-  </head>
-  <body>
-    <h1>ESP Pushbutton Web Server</h1>
-    <button class="button" id="btn1" onmousedown="toggleCheckbox('on');" ontouchstart="toggleCheckbox('on');" onmouseup="toggleCheckbox('off');" ontouchend="toggleCheckbox('off');">Forward</button>
-    <button class="button" id="btn2" onmousedown="toggleCheckbox('left');" ontouchstart="toggleCheckbox('left');" onmouseup="toggleCheckbox('off');" ontouchend="toggleCheckbox('off');">Left</button>
-    <button class="button" id="btn3" onmousedown="toggleCheckbox('right');" ontouchstart="toggleCheckbox('right');" onmouseup="toggleCheckbox('off');" ontouchend="toggleCheckbox('off');">Right</button>
-    <button class="button" id="btn4" onmousedown="toggleCheckbox('back');" ontouchstart="toggleCheckbox('back');" onmouseup="toggleCheckbox('off');" ontouchend="toggleCheckbox('off');">Backward</button>
+        body {
+            font-family: Arial;
+            text-align: center;
+            margin: 0px auto;
+            padding-top: 30px;
+        }
 
-   <script>
-   function toggleCheckbox(x) {
-     var xhr = new XMLHttpRequest();
-     xhr.open("GET", "/" + x, true);
-     xhr.send();
-   }
-  </script>
-  </body>
-</html>)rawliteral";
+        div.wrapper {
+            display: flex;
+            flex-direction: column;
+        }
+
+        div.rows {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .button {
+            font-weight: bold;
+            width: 20px;
+            padding: 15px 80px;
+            margin: 8px;
+            height: 160px;
+            font-size: 24px;
+            text-align: center;
+            outline: none;
+            color: #fff;
+            background-color: #2f4468;
+            border: none;
+            border-radius: 5px;
+            box-shadow: 0 6px #999;
+            cursor: pointer;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        }
+
+        .button:hover {
+            background-color: #1f2e45
+        }
+
+        .button:active {
+            background-color: #1f2e45;
+            box-shadow: 0 4px #666;
+            transform: translateY(2px);
+        }
+
+        button.top.edgeright {
+            border-radius: 5px 50px 5px 5px;
+        }
+
+        button.top.edgeleft {
+            border-radius: 50px 5px 5px 5px;
+        }
+
+        button.bot.edgeleft {
+            border-radius: 5px 5px 5px 50px;
+        }
+
+        button.bot.edgeright {
+            border-radius: 5px 5px 50px 5px;
+        }
+
+        div.movement {
+            display: flex;
+
+            justify-content: center;
+        }
+
+        div.row {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="controllers">
+            <div class="movement">
+                <div class="rows">
+                    <div class="top row">
+                        <button class="button top edgeleft" id="btn1" onmousedown="toggleCheckbox('forwleft');"
+                            ontouchstart="toggleCheckbox('forwleft');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">↖</button>
+                        <button class="button top mid" id="btn2" onmousedown="toggleCheckbox('on');"
+                            ontouchstart="toggleCheckbox('on');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">⬆</button>
+                        <button class="button top edgeright" id="btn3" onmousedown="toggleCheckbox('forwright');"
+                            ontouchstart="toggleCheckbox('forwright');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">↗</button>
+                    </div>
+                    <div class="mid row">
+                        <button class="button middle left" id="btn1" onmousedown="toggleCheckbox('left');"
+                            ontouchstart="toggleCheckbox('left');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">⬅</button>
+                        <span style="padding: 10px 80px; margin: 8px;"></span>
+                        <button class="button middle right" id="btn3" onmousedown="toggleCheckbox('right');"
+                            ontouchstart="toggleCheckbox('right');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">➡</button>
+                    </div>
+                    <div class="bot row">
+                        <button class="button bot edgeleft" id="btn1" onmousedown="toggleCheckbox('backleft');"
+                            ontouchstart="toggleCheckbox('backleft');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">↙</button>
+                        <button class="button bot mid" id="btn2" onmousedown="toggleCheckbox('back');"
+                            ontouchstart="toggleCheckbox('back');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">⬇</button>
+                        <button class="button bot edgeright" id="btn3" onmousedown="toggleCheckbox('backright');"
+                            ontouchstart="toggleCheckbox('backright');" onmouseup="toggleCheckbox('off');"
+                            ontouchend="toggleCheckbox('off');">↘</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1>ESP Pushbutton Web Server</h1>
+    <script>
+        function toggleCheckbox(x) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/" + x, true);
+            xhr.send();
+        }
+    </script>
+</body>
+
+</html>
+)rawliteral";
 
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
@@ -127,6 +216,16 @@ void setup() {
     request->send(200, "text/plain", "ok");
   });
 
+  server.on("/forwleft", HTTP_GET, [](AsyncWebServerRequest *request) {
+    transmit('d');
+    request->send(200, "text/plain", "ok");
+  });
+
+  server.on("/forwright", HTTP_GET, [](AsyncWebServerRequest *request) {
+    transmit('g');
+    request->send(200, "text/plain", "ok");
+  });
+
   server.on("/left", HTTP_GET, [](AsyncWebServerRequest *request) {
     transmit('l');
     request->send(200, "text/plain", "ok");
@@ -139,6 +238,16 @@ void setup() {
 
   server.on("/back", HTTP_GET, [](AsyncWebServerRequest *request) {
     transmit('b');
+    request->send(200, "text/plain", "ok");
+  });
+
+  server.on("/backleft", HTTP_GET, [](AsyncWebServerRequest *request) {
+    transmit('v');
+    request->send(200, "text/plain", "ok");
+  });
+
+  server.on("/backright", HTTP_GET, [](AsyncWebServerRequest *request) {
+    transmit('n');
     request->send(200, "text/plain", "ok");
   });
 
--- project/I2Ccommunication/Slave/Slave.ino
+++ project/I2Ccommunication/Slave/Slave.ino
@@ -30,6 +30,10 @@ int motorBB1 = 4;
 int motorBB2 = 3;
 int motorBBPWM = 2;
 
+int headerA1 = 47;
+int headerA2 = 43;
+int headerPWM = 45;
+
 void setup() {
   Serial.begin(9600);
   Wire.begin(8);  // join i2c bus with address #8
@@ -56,6 +60,11 @@ void setup() {
   pinMode(motorBB2, OUTPUT);
   pinMode(motorBBPWM, OUTPUT);
 
+  // Header
+  pinMode(headerA1, OUTPUT);
+  pinMode(headerA2, OUTPUT);
+  pinMode(headerPWM, OUTPUT);
+
   // Left Part || Forward part
   digitalWrite(motorA1, HIGH);
   digitalWrite(motorA2, LOW);
@@ -69,6 +78,10 @@ void setup() {
 
   digitalWrite(motorBB1, HIGH);
   digitalWrite(motorBB2, LOW);
+
+  // Header
+  digitalWrite(headerA1, HIGH);
+  digitalWrite(headerA2, LOW);
 }
 
 void loop() {
@@ -82,10 +95,10 @@ void loop() {
 //   // as expected by master
 // }
 
-//     analogWrite(motorAPWM, 127);
-//     analogWrite(motorBPWM, 127);
+//     analogWrite(motorAPWM, 255);
+//     analogWrite(motorBPWM, 255);
 
-//     analogWrite(motorBBPWM, 127);
+//     analogWrite(motorBBPWM, 255);
 
 //     digitalWrite(motorA1, LOW);
 //     digitalWrite(motorA2, HIGH);
@@ -97,10 +110,10 @@ void loop() {
 //     digitalWrite(motorBB2, HIGH);
 
 
-//     analogWrite(motorAPWM, 127);
-//     analogWrite(motorBPWM, 127);
+//     analogWrite(motorAPWM, 255);
+//     analogWrite(motorBPWM, 255);
 
-//     analogWrite(motorBBPWM, 127);
+//     analogWrite(motorBBPWM, 255);
 
 
 
@@ -112,7 +125,29 @@ void receiveEvent(size_t howMany) {
   }
   int x = Wire.read();
   Serial.println(x);
+  if (x == 100) {
+    Serial.println("Forward-left!!!");
 
+    // left side
+    digitalWrite(motorA1, LOW);
+    digitalWrite(motorA2, HIGH);
+
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, HIGH);
+
+    analogWrite(motorAPWM, 255);
+    analogWrite(motorBPWM, 255);
+
+    // right side
+    digitalWrite(motorAA1, HIGH);
+    digitalWrite(motorAA2, LOW);
+
+    digitalWrite(motorBB1, HIGH);
+    digitalWrite(motorBB2, LOW);
+
+    analogWrite(motorAAPWM, 255);
+    analogWrite(motorBBPWM, 255);
+  }
   if (x == 102) {
     Serial.println("Forward!!!");
 
@@ -123,22 +158,23 @@ void receiveEvent(size_t howMany) {
     digitalWrite(motorB1, LOW);
     digitalWrite(motorB2, HIGH);
 
-    analogWrite(motorAPWM, 127);
-    analogWrite(motorBPWM, 127);
+    analogWrite(motorAPWM, 180);
+    analogWrite(motorBPWM, 180);
 
     // right side
-    digitalWrite(motorAA1, HIGH);
-    digitalWrite(motorAA2, LOW);
+    digitalWrite(motorAA1, LOW);
+    digitalWrite(motorAA2, HIGH);
 
     digitalWrite(motorBB1, LOW);
     digitalWrite(motorBB2, HIGH);
 
-    analogWrite(motorAAPWM, 127);
-    analogWrite(motorBBPWM, 127);
+    analogWrite(motorAAPWM, 230);
+    analogWrite(motorBBPWM, 230);
   }
-  if (x == 108) {
-    Serial.println("Left!!!");
+  if (x == 103) {
+    Serial.println("Forward-right!!!");
 
+    
     // left side
     digitalWrite(motorA1, HIGH);
     digitalWrite(motorA2, LOW);
@@ -146,8 +182,40 @@ void receiveEvent(size_t howMany) {
     digitalWrite(motorB1, HIGH);
     digitalWrite(motorB2, LOW);
 
-    analogWrite(motorAPWM, 127);
-    analogWrite(motorBPWM, 127);
+    analogWrite(motorAPWM, 255);
+    analogWrite(motorBPWM, 255);
+
+    // right side
+    digitalWrite(motorAA1, LOW);
+    digitalWrite(motorAA2, HIGH);
+
+    digitalWrite(motorBB1, LOW);
+    digitalWrite(motorBB2, HIGH);
+
+    analogWrite(motorAAPWM, 255);
+    analogWrite(motorBBPWM, 255);
+  }
+
+  /*
+  right - l1 -back, l2 -forw, r1 -forw, r2 -back
+  left - l1 -forw, l2 -back, r1 -back, r2 -forw
+  */
+  if (x == 108) {
+    Serial.println("Left!!!");
+
+    // digitalWrite(headerA1, HIGH);
+    // digitalWrite(headerA2, LOW);
+    // analogWrite(headerPWM, 0);
+
+    // left side
+    digitalWrite(motorA1, HIGH);
+    digitalWrite(motorA2, LOW);
+
+    digitalWrite(motorB1, LOW);
+    digitalWrite(motorB2, HIGH);
+
+    analogWrite(motorAPWM, 255);
+    analogWrite(motorBPWM, 255);
 
     // right side
 
@@ -157,32 +225,57 @@ void receiveEvent(size_t howMany) {
     digitalWrite(motorBB1, LOW);
     digitalWrite(motorBB2, HIGH);
 
-    analogWrite(motorAAPWM, 127);
-    analogWrite(motorBBPWM, 127);
+    analogWrite(motorAAPWM, 255);
+    analogWrite(motorBBPWM, 255);
   }
   if (x == 114) {
-    Serial.println("Left!!!");
+    Serial.println("Right!!!");
 
     // left side
     digitalWrite(motorA1, LOW);
     digitalWrite(motorA2, HIGH);
 
-    digitalWrite(motorB1, LOW);
-    digitalWrite(motorB2, HIGH);
+    digitalWrite(motorB1, HIGH);
+    digitalWrite(motorB2, LOW);
 
-    analogWrite(motorAPWM, 127);
-    analogWrite(motorBPWM, 127);
+    analogWrite(motorAPWM, 255);
+    analogWrite(motorBPWM, 255);
 
     // right side
 
-    digitalWrite(motorAA1, LOW);
-    digitalWrite(motorAA2, HIGH);
+    digitalWrite(motorAA1, HIGH);
+    digitalWrite(motorAA2, LOW);
+
+    digitalWrite(motorBB1, LOW);
+    digitalWrite(motorBB2, HIGH);
+
+    analogWrite(motorAAPWM, 255);
+    analogWrite(motorBBPWM, 255);
+  }
+  if (x == 118) {
+    Serial.println("Back-left!!!");
+
+    
+    // left side
+    digitalWrite(motorA1, HIGH);
+    digitalWrite(motorA2, LOW);
+
+    digitalWrite(motorB1, HIGH);
+    digitalWrite(motorB2, LOW);
+
+    analogWrite(motorAPWM, 0);
+    analogWrite(motorBPWM, 255);
+
+    // right side
+
+    digitalWrite(motorAA1, HIGH);
+    digitalWrite(motorAA2, LOW);
 
     digitalWrite(motorBB1, HIGH);
     digitalWrite(motorBB2, LOW);
 
-    analogWrite(motorAAPWM, 127);
-    analogWrite(motorBBPWM, 127);
+    analogWrite(motorAAPWM, 255);
+    analogWrite(motorBBPWM, 0);
   }
   if (x == 98) {
     Serial.println("Backward!!!");
@@ -195,21 +288,50 @@ void receiveEvent(size_t howMany) {
     digitalWrite(motorB1, HIGH);
     digitalWrite(motorB2, LOW);
 
-    analogWrite(motorAPWM, 127);
-    analogWrite(motorBPWM, 127);
+    analogWrite(motorAPWM, 255);
+    analogWrite(motorBPWM, 255);
 
     // right side
 
-    digitalWrite(motorAA1, LOW);
-    digitalWrite(motorAA2, HIGH);
+    digitalWrite(motorAA1, HIGH);
+    digitalWrite(motorAA2, LOW);
 
     digitalWrite(motorBB1, HIGH);
     digitalWrite(motorBB2, LOW);
 
-    analogWrite(motorAAPWM, 127);
-    analogWrite(motorBBPWM, 127);
+    analogWrite(motorAAPWM, 230);
+    analogWrite(motorBBPWM, 230);
+  }
+  if (x == 110) {
+    Serial.println("Back-right!!!");
+    
+    // left side
+    digitalWrite(motorA1, HIGH);
+    digitalWrite(motorA2, LOW);
+
+    digitalWrite(motorB1, HIGH);
+    digitalWrite(motorB2, LOW);
+
+    analogWrite(motorAPWM, 255);
+    analogWrite(motorBPWM, 0);
+
+    // right side
+
+    digitalWrite(motorAA1, HIGH);
+    digitalWrite(motorAA2, LOW);
+
+    digitalWrite(motorBB1, HIGH);
+    digitalWrite(motorBB2, LOW);
+
+    analogWrite(motorAAPWM, 0);
+    analogWrite(motorBBPWM, 255);
   }
   if (x == 101) {
+    // header
+    digitalWrite(headerA1, HIGH);
+    digitalWrite(headerA2, LOW);
+
+    analogWrite(headerPWM, 0);
     // left side
     digitalWrite(motorA1, HIGH);
     digitalWrite(motorA2, LOW);
